708f2ac7bcf9347cdbfb711ec2e43cee1e3b9dbb
darktab <christian.jacoby@a3.epfl.ch>
2014-04-07 13:27:57 +0200

    Added TDataListBox

---
+++ UDataListBox.pas
@@ -0,0 +1,82 @@
+unit UDataListBox;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FMX.Types, FMX.Controls, FMX.Layouts,
+  FMX.ListBox, Data.DB;
+
+type
+  TDataListBox = class(TListBox)
+  private
+    { Private declarations }
+    fDataSet: TDataSet;
+    fDataFieldName: String;
+  protected
+    { Protected declarations }
+  public
+    { Public declarations }
+    procedure init(lDataSet: TDataSet; lDataFieldName: string); overload;
+    procedure init; overload;
+    Constructor Create(AOwner: TComponent); override;
+  published
+    { Published declarations }
+    Property DataSet: TDataSet read fDataSet write fDataSet;
+    Property DataFieldName: String read fDataFieldName write fDataFieldName;
+  end;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterComponents('Data Access', [TDataListBox]);
+end;
+
+procedure TDataListBox.init;
+var
+  field: TField;
+begin
+  self.Items.Clear;
+  if not self.DataSet.Active then
+    self.DataSet.Open;
+  self.DataSet.First;
+  while not self.DataSet.EOF do
+  begin
+    for field in self.DataSet.Fields do
+    begin
+      if field.FieldName = self.DataFieldName then
+      begin
+        self.Items.Add(self.DataSet.FieldByName(field.FieldName).AsString);
+      end;
+    end;
+    self.DataSet.Next;
+  end;
+
+  // on se positionne sur le premier bouquet
+  // et on retourne la référence
+  self.ItemIndex := 0;
+  self.DataSet.First;
+end;
+
+procedure TDataListBox.init(lDataSet: TDataSet; lDataFieldName: string);
+begin
+  self.DataSet := lDataSet;
+  self.DataFieldName := lDataFieldName;
+
+  init;
+
+end;
+
+// Create a ListView
+constructor TDataListBox.Create(AOwner: TComponent);
+begin
+  // Execute the parent (TObject) constructor first
+  inherited; // Call the parent Create method
+
+  // init;
+
+end;
+
+end.
